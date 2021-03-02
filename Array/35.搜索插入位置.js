5aa89cbcdf3439cdb9ef22a72b8beedcdf224a24
cs24k1993 <1344529446@qq.com>
2021-03-02 20:51:56 +0800

    2021/3/2 Update

--- Array/35.搜索插入位置.js
+++ Array/35.搜索插入位置.js
@@ -1,7 +1,8 @@
 // 二分查找
 function searchInsert(nums, target) {
   let l = 0;
-  let r = nums.length-1;
+  // r的值不能是 nums.length - 1（因为可能插入到数组最后的位置）
+  let r = nums.length;
   while (l < r) {
     let mid = Math.floor((l + r) / 2);
     if (nums[mid] >= target) {
@@ -14,3 +15,4 @@ function searchInsert(nums, target) {
 }
 
 console.log(searchInsert([1,2,5], 3));
+console.log(searchInsert([1,3,5,6], 7));
