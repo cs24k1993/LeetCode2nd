93c9016378acfb87cfd0acfb5c7d0dddec75d3c0
cs24k1993 <1344529446@qq.com>
2021-03-16 19:57:19 +0800

    2021/3/16 Update

--- Array/119.杨辉三角二.js
+++ Array/119.杨辉三角二.js
@@ -1,7 +1,7 @@
 // 递推，C[i]表示一整行
 function getRow(rowIndex) {
   const C = new Array(rowIndex+1).fill(0);
-  for( let i = 0; i <=rowIndex + 1; ++i) {
+  for( let i = 0; i <=rowIndex + 1; i++) {
     C[i] = new Array(i + 1).fill(0);
     C[i][0] = C[i][i] = 1;
     for (let j = 1; j < i; j++) {
